125379f47d317635bfe2006c7b2da31a459cec78
john klees <johnnynl@live.nl>
2019-04-21 13:09:30 +0200

    aantal dingen toegevoegd

--- src/Examen.java
+++ src/Examen.java
@@ -3,10 +3,9 @@ import java.util.ArrayList;
 public class Examen {
     private String soortExamen;
     private String examenNaam;
-    private ArrayList<Studenten> studenten = new ArrayList<>();
+    private ArrayList<Student> studenten = new ArrayList<>();
 
     public Examen(String examenNaam){
-
         this.examenNaam = examenNaam;
     }
 
@@ -15,6 +14,9 @@ public class Examen {
         this.soortExamen = soortExamen;
     }
 
+    public void addStudentToExamen(Student student){
+        this.studenten.add(student);
+    }
     @Override
     public String toString() {
         if(soortExamen == null){
--- src/Studenten.java
+++ src/Studenten.java
@@ -1,21 +1,23 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Studenten{
     private ArrayList<Student> Studenten = new ArrayList<>();
+    private Scanner reader = new Scanner(System.in);
 
     public void addStudent(Student student) {
         this.Studenten.add(student);
     }
-    public void VerwijderenStudenten() {
-
-        System.out.println("welke woorden wilt u verwijderen? ");
+    public void verwijderStudenten() {
+        System.out.println("Welke student wilt u verwijderen?");
         Scanner reader = new Scanner(System.in);
         String verwijderen = reader.nextLine();
         for (int i = 0; i < Studenten.size(); i++) {
-            if (Studenten.get(i).getStudentNaam().equals(verwijderen)) {
+            if (Studenten.get(i).getStudentNaam().contains(verwijderen)) {
                 Studenten.remove(i);
-
+            }else if (Studenten.get(i).getStudentNummer().contains(verwijderen)){
+                Studenten.remove(i);
             }
         }
         printStudenten();
@@ -24,6 +26,50 @@ public class Studenten{
         for (Student student : this.Studenten) {
             System.out.println(student);
         }
+
+    }
+    public Student voegStudentToe(){
+        Menu menu = new Menu();
+        String StudentNaam = "";
+        String StudentNummer = "";
+        System.out.println("Voeg een student toe");
+        System.out.println("--------------------");
+        System.out.print("Naam: ");
+        while(StudentNaam.equals("")){
+            try {
+                StudentNaam = reader.nextLine();
+            }catch (Exception e){
+                System.out.println("Iets ging mis.");
+            }
+            for (Student student: this.Studenten) {
+                if(student.getStudentNaam().toLowerCase().equals(StudentNaam.toLowerCase())){
+                    System.out.println("Deze student bestaat al.");
+                    System.out.print("Naam: ");
+                    StudentNaam = "";
+                }
+            }
+        }
+        System.out.print("Student nummer: ");
+        while(StudentNummer.equals("")){
+            try {
+                StudentNummer = reader.nextLine();
+            }catch (Exception e){
+                System.out.println("Iets ging mis.");
+            }
+            if(StudentNummer.length() < 6){
+                System.out.println("Student nummer moet groter dan 6 zijn.");
+                System.out.print("Student nummer: ");
+                StudentNummer = "";
+            }
+            for (Student student: this.Studenten) {
+                if(student.getStudentNummer().equals(StudentNummer)){
+                    System.out.println("Een student heeft dit nummer al.");
+                    System.out.print("Student nummer: ");
+                    StudentNummer = "";
+                }
+            }
+        }
+        return new Student(StudentNaam, StudentNummer);
     }
 }
 
